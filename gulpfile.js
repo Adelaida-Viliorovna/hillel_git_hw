5deeb85b2384f7633f55a6e8bca65b5aabea8ad3
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2024-08-09 18:01:18 +0300

    upd gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const { watch, series } = gulp;
+const { series } = gulp;
 const cssnano = require('gulp-cssnano');
 const sass = require('gulp-sass')(require('sass'));
 
@@ -12,16 +12,20 @@ gulp.task('sass', function() {
     return gulp.src('app/scss/style.scss')
            .pipe(sass().on('error', sass.logError))
            .pipe(cssnano())
-           .pipe(gulp.dest('dist/css'))
-});
-
-gulp.task('watch', function() {
-    gulp.watch('app/scss/*.scss', gulp.series('sass'))
+           .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('webp', async function() {
     const webp = await loadWebp();
     return gulp.src('app/img/*.jpg')
            .pipe(webp())
-           .pipe(gulp.dest('dist/images'))
+           .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('watch', function() {
+    // Спостерігає за змінами в SCSS файлах і виконує 'sass'
+    gulp.watch('app/scss/*.scss', gulp.series('sass'));
+
+    // Спостерігає за додаванням нових .jpg файлів і виконує 'webp'
+    gulp.watch('app/img/*.jpg', gulp.series('webp'));
 });
